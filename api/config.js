0b5ad92e0f759d5f6fea8281098d42a252cc12a5
Narendra Mandadapu <scriptstar@hotmail.co.uk>
2013-05-05 20:54:05 +0100

    Connection details changed

--- api/config.js
+++ api/config.js
@@ -1,9 +1,5 @@
 // Don't commit this file to your public repos
 exports.creds = {
-  // Your mongo auth uri goes here
-  // e.g. mongodb://username:server@mongoserver:28017/somecollection
-  //mongodb:'//localhost/messages',
-    //mongodb:'//scriptstar:aX6qQhQM@ds035787.mongolab.com:35787/nodecellar',
-  //mongoose_auth: 'mongodb://scriptstar:qwerty12345@ds035787.mongolab.com:35787/nodecellar'
-    mongoose_auth: 'mongodb://localhost:27017/invitify'
+// Your mongo auth uri goes here
+mongoose_auth: 'mongodb://scriptstar:qwerty12345@ds035787.mongolab.com:35787/invitify'
 }
