3a8dff293e262e57b03e5e54436617596bd8a500
Narendra Mandadapu <scriptstar@hotmail.co.uk>
2013-05-05 21:01:22 +0100

    Connection Details Changed again!

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 // Don't commit this file to your public repos
 exports.creds = {
 // Your mongo auth uri goes here
-mongoose_auth: 'mongodb://scriptstar:qwerty12345@ds035787.mongolab.com:35787/invitify'
+mongoose_auth: 'mongodb://invitify:1nv1t1$y@ds053497.mongolab.com:53497/invitify'
 }
